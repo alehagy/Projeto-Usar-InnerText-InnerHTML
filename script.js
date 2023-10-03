cdd11d7dae0478a8f3320b50edd6bb7d2cebe2cf
alessandro <ale_hagy@hotmail.com>
2023-10-03 16:46:22 -0300

    1o.commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//resgata h1
+//let elementoh1 = document.querySelector ("h1")
+//console.log(elementoh1)
+
+//resgata ul
+//let elementoul = document.querySelector ("ul")
+//console.log(elementoul)
+
+//resgata a
+//let elementoa = document.querySelector ("a")
+//console.log(elementoa)
+
+//resgata ol
+//let elementool = document.querySelector ("ol")
+//console.log(elementool)
+
+//resgata conteudo h1
+//let elementoh1 = document.querySelector ("h1");
+//console.log(elementoh1);
+//console.log(elementoh1.innerText);
+
+//altera conteudo h1
+//let elementoh1 = document.querySelector ("h1");
+//elementoh1.innerText= "Novo titulo com JS";
+
+//altera conteudo a
+//let a = document.querySelector ("a");
+//a.ref= "https://www.hotmail.com";
+//console.log(a);
+
+//altera conteudo ul
+//let elementoul = document.querySelector ("ul")
+//elementoul.innerHTML = "<ul><li>Elemento de lista JS 01</li><li>Elemento de lista JS 02</li><li>Elemento de lista JS 03</li></ul>"
+
+//console.log(elementoul)
+
+//altera conteudo ol
+let elementol = document.querySelector ("ol")
+elementol.innerHTML = "<ol><a href="https://facebook.com"></a></ol><ol><a href="https://youtube.com"></a></ol><ol><a href="https://oracle.com"></a></ol>"
+
+console.log(ol)
